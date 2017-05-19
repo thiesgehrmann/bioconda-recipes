f1db610478251f982076a72c0650a915585238dc
thiesgehrmann <thiesgehrmann@gmail.com>
2017-05-19 13:09:15 +0200

    removed orthofinder-dl, for the moment

--- recipes/orthofinder-dl/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/opt/orthofinder-dl
-
-cp -R orthofinder/* $PREFIX/opt/orthofinder-dl
-
-ln -s $PREFIX/opt/orthofinder-dl/orthofinder.py $PREFIX/bin/orthofinder-dl.py
