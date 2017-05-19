8265bca846f9814ebc908dc13e3752c8395dead2
thiesgehrmann <thiesgehrmann@gmail.com>
2017-05-19 12:46:10 +0200

    Added orthofinder-dl. Needs to be built with --python 2.7

---
+++ recipes/orthofinder-dl/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pwd
+ls
+
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/opt/orthofinder-dl
+
+cp -R orthofinder/* $PREFIX/opt/orthofinder-dl
+
+ln -s $PREFIX/opt/orthofinder-dl/orthofinder.py $PREFIX/bin/orthofinder-dl.py
